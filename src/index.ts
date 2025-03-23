fb5b995f7deb44168f6c70941b8b38677f8bcb7c
Yosuke Yamashita <yosuke@aka.jp>
2025-03-23 12:27:53 +0900

    run controller only on develepment

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import "./scss/main.scss";
 import { AkariLogo } from "./ts/view/logo/Logo";
 import { Controller } from "./ts/control/Controller";
 
-window.onload = (): void => {
-  new Controller();
-};
+// TODO: 環境変数でテストかどうかを判断して、テストの場合はControllerを実行する
+// window.onload = (): void => {
+//   new Controller();
+// };
 
 export { AkariLogo };
