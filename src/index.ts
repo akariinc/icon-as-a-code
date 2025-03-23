23e0f7ccb0d50b112abb20a1ee7731e2ad21d965
Yosuke Yamashita <yosuke@aka.jp>
2025-03-23 12:32:46 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import { AkariLogo } from "./ts/view/logo/Logo";
 import { Controller } from "./ts/control/Controller";
 
 // TODO: 環境変数でテストかどうかを判断して、テストの場合はControllerを実行する
-// window.onload = (): void => {
-//   new Controller();
-// };
+if ("LOGO_DEV_ENV" in window && window["LOGO_DEV_ENV"] !== "test") {
+  window.onload = (): void => {
+    new Controller();
+  };
+}
 
 export { AkariLogo };
